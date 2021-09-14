de0b38870972ebf0d15b47ee05811c18738bb0d5
Codesaurus <85794164+code-saurus@users.noreply.github.com>
2021-09-14 12:41:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+/**
+ * @INFO
+ * Bot Coded by  | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention Him / Milrato Development, when using this Code!
+ * @INFO
+ */
+
+//Importing all needed Commands
+const Discord = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
+const colors = require("colors"); //this Package is used, to change the colors of our Console! (optional and doesnt effect performance)
+const Enmap = require("enmap"); //this package is our Database! We will use it to save the data for ever!
+const fs = require("fs"); //this package is for reading files and getting their inputs
+const Emoji = require("./botconfig/emojis.json")
+const config = require("./botconfig/config.json")
+//Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
+const client = new Discord.Client({
+  //  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  //  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  shards: "auto",
+  disableEveryone: true,
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  presence: {
+    afk: false,
+    activity: {
+      name: `${require("./botconfig/config.json").status.text}`.replace("{prefix}", require("./botconfig/config.json").prefix), 
+      type: require("./botconfig/config.json").status.type, 
+      url: require("./botconfig/config.json").status.url
+    },
+    status: "online"
+  }
+});
+
+client.setMaxListeners(50);
+require('events').defaultMaxListeners = 50;
+
+
+
+const Meme = require("memer-api");
+client.memer = new Meme("PASTE A TOKEN HERE"); //GET IT HERE: https://discord.gg/Mc2FudJkgP
+
+
+client.adenabled = true;
+client.statusad = {
+  name: `a_random_machine`,
+  type: "PLAYING", 
+  url: "https://twitch.tv/#"
+};
+client.spacedot = "・";
+client.textad = "a_random_machine";
+
+
+//Loading discord-buttons
+const dbs = require('discord-buttons');
+dbs(client);
+
+function requirehandlers(){
+  client.basicshandlers = Array(
+    "extraevents", "loaddb", "clientvariables", "command", "events", "erelahandler"
+  );
+  client.basicshandlers.forEach(handler => {
+    try{ require(`./handlers/${handler}`)(client); }catch (e){ console.log(e) }
+  });
+}requirehandlers();
+
+function requiresociallogs(){
+  client.socialhandlers = Array(
+    "twitterfeed", /*"twitterfeed2",*/ "livelog", "youtube", "tiktok"
+  );
+  client.socialhandlers.forEach(handler=>{
+    try{ require(`./social_log/${handler}`)(client); }catch (e){ console.log(e) }
+  })
+}requiresociallogs();
+
+function requireallhandlers(){
+  client.allhandlers = Array(
+    "apply", "apply2", "apply3", "apply4", "apply5",
+    "ticket", "ticket2", "ticket3", "ticket4", "ticket5",
+    "roster", "roster2", "roster3",
+    "welcome", "leave",
+    "jointocreate", "logger", "reactionrole", "ranking",
+    "antidiscord", "antilinks","anticaps", "blacklist", "keyword",
+    "membercount", "autoembed", "suggest", "validcode", "dailyfact", "autonsfw",
+    "aichat"
+  )
+  client.allhandlers.forEach(handler => {
+    try{ require(`./handlers/${handler}`)(client); }catch (e){ console.log(e) }
+  });
+}requireallhandlers();
+
+//login to the Bot
+client.login(require("./botconfig/config.json").token);
+
+module.exports.requirehandlers = requirehandlers;
+module.exports.requiresociallogs = requiresociallogs;
+module.exports.requireallhandlers = requireallhandlers;
+
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention Him / Milrato Development, when using this Code!
+ * @INFO
+ */
